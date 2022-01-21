c9ecf90bc80f1cfe4db7d7c6b7d9ed52bf5143f1
Aniket Kale <57555096+Aniket-d-d@users.noreply.github.com>
2022-01-21 07:22:24 +0530

    Update Aniket_Kale_LAB2_Excercise_1.sql

--- Aniket_Kale_LAB2_Excercise_1.sql
+++ Aniket_Kale_LAB2_Excercise_1.sql
@@ -1,1 +1,14 @@
 # GCP-LAB-2-Aniket-Kale
+WITH movie_detail AS(
+SELECT year,Movie_Title,Production_Budget,RANK() OVER(PARTITION BY year order by Production_Budget DESC) AS rank
+FROM (
+SELECT
+EXTRACT(YEAR FROM Release_Date) AS year,Movie_Title,Production_Budget
+FROM `uc1_17.Movie`
+WHERE Release_Date BETWEEN '2016-01-01' and '2020-12-31')
+)
+SELECT
+year,Movie_Title,Production_Budget,rank
+FROM movie_detail
+WHERE rank <= 10
+ORDER BY year DESC
